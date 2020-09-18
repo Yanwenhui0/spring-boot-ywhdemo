99b455e3f3d62b455c39e6990ba1abfa3793f6be
yanwenhui <1697479326@qq.com>
2020-09-18 16:12:15 +0800

    spring-export-file.

---
+++ spring-export-file/src/main/java/com/springboot/exportfile/SpringExportFileApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.exportfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExportFileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringExportFileApplication.class, args);
+    }
+
+}
